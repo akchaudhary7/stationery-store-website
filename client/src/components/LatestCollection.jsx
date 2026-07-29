53d8d28e1cc4e230a49798b643bb76cc43c3d1cd
Amit Chaudhary <ak.chaudhary8125@gmail.com>
2026-07-29 10:24:41 +0545

    feat: enhance layout of Best Seller and Latest Collection components for improved responsiveness

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -16,17 +16,17 @@ const LatestCollection = () => {
 
 
     return (
-        <div className='mt-10 mx-35'>
+        <div className='mt-10 mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
             <div className='text-center py-8 text-3xl'>
                 <Title text1={'LATEST'} text2={'PRODUCTS'} />
-                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                <p className='w-full sm:w-3/4 md:w-2/3 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam adipisci, assumenda omnis amet ad exercitationem veritatis porro corporis. Recusandae tenetur
                 </p>
             </div>
 
             {/* Rendering Products */}
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
                 {latestProducts.map((item) => (
                     <Item
                         key={item._id}
